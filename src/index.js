e644ba5a989fa170ff291f7938542ae85fc45aa6
Rhuan-Rodrigues1 <gitrhuan34@gmail.com>
2021-12-29 14:56:21 -0300

    adding more styles

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const toggle = document.querySelectorAll('nav .toggle')
 const links = document.querySelectorAll('nav ul li a')
 const header = document.querySelector('#header')
 
+
 for (const el of toggle) {
   el.addEventListener('click', () => {
     nav.classList.toggle('show')
@@ -24,3 +25,29 @@ window.addEventListener('scroll', () => {
     header.classList.remove('scroll')
   }
 })
+
+const swiper = new Swiper('.swiper',{
+  slidesPerView: 1,
+  pagination: {
+    el: '.swiper-pagination',
+    type: 'bullets',
+  },
+  mousewheel: true,
+  keyboard: true
+})
+
+const scrollReveal = ScrollReveal({
+  origin: 'top',
+  distance: '30px',
+  duration: 700,
+  reset: true
+})
+scrollReveal.reveal(`
+#home .image, #home .text,
+#about .image, #about .text,
+#services header, #services card,
+#testmonials header, #testmonials testmonials,
+#contacts .text, #contacts .links
+
+`, {interval: 100})
+
